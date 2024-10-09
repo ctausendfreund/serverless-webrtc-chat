db9eb55e4c1910eeaa98613ce09d36102964e4d9
Christian Tausendfreund <c.tausendfreund@micromata.de>
2024-10-09 10:08:12 +0200

    fix a warning about modifying RTCSessionDescription members

--- common.js
+++ common.js
@@ -1,31 +1,31 @@
 function replaceCandidateHostnamesWithLocalhost(sdpObject) {
-    sdpObject.sdp = sdpObject.sdp.split('\n').map(line => {
-        if (line.startsWith('a=candidate')) {
-            const parts = line.split(' ');
-            if (parts.length > 4) {
-                parts[4] = '127.0.0.1';
-            }
-            return parts.join(' ');
-        }
-        return line;
-    }).join('\n');
+  const modifiedSdp = sdpObject.sdp.split('\n').map(line => {
+    if (line.startsWith('a=candidate')) {
+      const parts = line.split(' ');
+      if (parts.length > 4) {
+        parts[4] = '127.0.0.1';
+      }
+      return parts.join(' ');
+    }
+    return line;
+  }).join('\n');
 
-    return sdpObject;
+  return new RTCSessionDescription({type: sdpObject.type, sdp: modifiedSdp});
 }
 
 function addMSG(msg, who) {
-    const wrap = $("<div>").addClass("wrap").appendTo($("#chat-screen"));
-    const div  = $("<div>").addClass(who).appendTo(wrap);
-    $("<span>").html(who).addClass("who").appendTo(div);
-    $("<span>").html(msg).addClass("msg").appendTo(div);
-    $("#chat-screen-wp").scrollTop($("#chat-screen").height());
+  const wrap = $("<div>").addClass("wrap").appendTo($("#chat-screen"));
+  const div = $("<div>").addClass(who).appendTo(wrap);
+  $("<span>").html(who).addClass("who").appendTo(div);
+  $("<span>").html(msg).addClass("msg").appendTo(div);
+  $("#chat-screen-wp").scrollTop($("#chat-screen").height());
 }
 
 function sendMSG(dc) {
-    const value = $("#msg").val();
-    if (value) {
-        dc.send(value);
-        addMSG(value, "me");
-        $("#msg").val('');
-    }
+  const value = $("#msg").val();
+  if (value) {
+    dc.send(value);
+    addMSG(value, "me");
+    $("#msg").val('');
+  }
 }
